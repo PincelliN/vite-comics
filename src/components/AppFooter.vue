a4607dae7298537bb2235c1021cd58bf77cd1f0f
Nicolò Pincelli <pincelli.n@gmail.com>
2024-07-06 20:27:24 +0200

    add list footer genereted with  v-for

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,53 +3,160 @@ export default {
   name: "AppFooter",
   data() {
     return {
-      links: [{
-        text: "characters",
-        url: "#",
-        state: false,
-      },
-      {
-        text: "comics",
-        url: "#",
-        state: true,
-      },
-      {
-        text: "movies",
-        url: "#",
-        state: false,
-      },
-      {
-        text: "tv",
-        url: "#",
-        state: false,
-      },
-      {
-        text: "games",
-        url: "#",
-        state: false,
-      },
-      {
-        text: "collectibles",
-        url: "#",
-        state: false,
-      },
-      {
-        text: "videos",
-        url: "#",
-        state: false,
-      },
-      {
-        text: "fans",
-        url: "#",
-        state: false,
-      },
-      {
-        text: "news",
-        url: "#",
-        state: false,
-      },
+      shopAreas: [{
+        area: "Dc Comics",
+        links: [{
+          text: "characters",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "comics",
+          url: "#",
+          state: true,
+        },
+        {
+          text: "movies",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "tv",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "games",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "collectibles",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "videos",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "fans",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "news",
+          url: "#",
+          state: false,
+        },
 
-      ]
+        ]
+      },
+      {
+        area: "Shop",
+        links:
+          [{
+            text: "Shop Dc",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Shop Dc Collectibles",
+            url: "#",
+            state: true,
+          }]
+      },
+      {
+        area: "Dc",
+        links:
+          [{
+            text: "Terms of Use",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Privacy policy(New)",
+            url: "#",
+            state: true,
+          },
+          {
+            text: "Ad Choices",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Advertising",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Jobs",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Subscriptions",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Talent Workshops",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "CPSC Certificateas",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Rating",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Shop Help",
+            url: "#",
+            state: false,
+          },
+          {
+            text: "Contacts Us",
+            url: "#",
+            state: false,
+          }]
+      },
+      {
+        area: "Sites",
+        links: [{
+          text: "Dc",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "MAD Magazine",
+          url: "#",
+          state: true,
+        },
+        {
+          text: "Dc Kids",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "Dc Universe",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "Dc Power Visa",
+          url: "#",
+          state: false,
+        }
+        ]
+      }
+      ],
+
     }
   }
 }
@@ -57,43 +164,10 @@ export default {
 <template>
   <div class="back">
     <div class="list-box">
-      <div>
-        <h4>dc comics</h4>
+      <div v-for="shoparea in shopAreas">
+        <h4>{{ shoparea.area }}</h4>
         <ul>
-          <li v-for="link in links"> <a :href="link.url"></a>{{ link.text }}</li>
-        </ul>
-        <div>
-          <h4>Shop</h4>
-          <ul>
-            <li><a href="#"></a>Shop DC</li>
-            <li><a href="#"></a>Shop DC collectibles</li>
-          </ul>
-        </div>
-      </div>
-      <div>
-        <h4>dc</h4>
-        <ul>
-          <li><a href="#"></a>Terms of Use</li>
-          <li><a href="#"></a>Privacy policy(New)</li>
-          <li><a href="#"></a>Ad Choices</li>
-          <li><a href="#"></a>Advertising</li>
-          <li><a href="#"></a>Jobs</li>
-          <li><a href="#"></a>Subscriptions</li>
-          <li><a href="#"></a>Talent Workshops</li>
-          <li><a href="#"></a>CPSC Certificateas</li>
-          <li><a href="#"></a>Rating</li>
-          <li><a href="#"></a>Shop Help</li>
-          <li><a href="#"></a>Contacts Us</li>
-        </ul>
-      </div>
-      <div>
-        <h4>sites</h4>
-        <ul>
-          <li><a href="#"></a>DC</li>
-          <li><a href="#"></a>MAD Magazine</li>
-          <li><a href="#"></a>Dc Kids</li>
-          <li><a href="#"></a>DC Universe</li>
-          <li><a href="#"></a>DC Power Visa</li>
+          <li v-for="link in shoparea.links"> <a :href="link.url"></a>{{ link.text }}</li>
         </ul>
       </div>
     </div>
@@ -133,28 +207,39 @@ export default {
     background-position: 100% 50%;
     background-repeat: no-repeat;
     display: flex;
+    flex-direction: column;
     justify-content: flex-start;
+    align-items: flex-start;
+    align-content: flex-start;
+    flex-wrap: wrap;
+    height: 294px;
     width: 80%;
     margin: 0 auto;
     padding: 10px;
     gap: 10px;
 
+
+
     div {
       margin-top: 10px;
+
+
+
 
       h4 {
         color: white;
         text-transform: uppercase;
         font-weight: bolder;
-        text-align: start;
         margin-bottom: 5px;
       }
 
       ul {
         color: rgb(168, 168, 168);
         list-style: none;
-        text-align: start;
         text-transform: capitalize;
+
+
+
       }
     }
   }
@@ -194,14 +279,14 @@ footer {
         justify-content: flex-start;
         list-style: none;
         align-items: center;
+        gap: 8px;
+        padding: 10px;
 
-        li {
-          margin: 1rem;
 
-          img {
-            width: 25px;
-          }
+        img {
+          width: 25px;
         }
+
       }
     }
   }
